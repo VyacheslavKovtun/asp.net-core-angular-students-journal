733d7e58dda996257bf7ad84ace32667ca4c4c44
Vyacheslav Kovtun <kovtyn11191@gmail.com>
2021-07-31 11:59:17 +0300

    db architecture

---
+++ WorkApp/Database/Common/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkApp.Database.Common
+{
+    public class DatabaseContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
